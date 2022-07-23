013819b42ad8eb0ac610276d2b54ece0fc305d39
jason <binglong2206@gmail.com>
2022-07-24 04:04:24 +0700

    facebook instagram youtube all migrated

--- src/routes/facebook.ts
+++ src/routes/facebook.ts
@@ -1,5 +1,5 @@
 import express, { NextFunction, Request, Response } from "express";
-import { verifyHeaderJWT } from "../middleware/verifyJWT";
+import { verifyCookieJWT, verifyHeaderJWT } from "../middleware/verifyJWT";
 import { updateFB } from "../controller/updateFB";
 import aggregateStat from "../controller/aggregateStat";
 import {fbURL, getCode, getAccessToken, getPageId, getPageAccessToken, getBasicStats, getMedias, getDemoGeo, getIntervalsData, postFBData} from '../utils/facebook_utils'
@@ -14,7 +14,7 @@ router.get("/", (req: Request, res: Response) => {
 
 
 
-router.get('/redirect', async (req: Request, res: Response, next: NextFunction) => {
+router.get('/redirect', verifyCookieJWT, async (req: Request, res: Response, next: NextFunction) => {
   const { accessToken, refreshToken } = cookie.parse(req.headers.cookie)
 
   res.setHeader("Set-Cookie", [
@@ -33,20 +33,32 @@ router.get('/redirect', async (req: Request, res: Response, next: NextFunction) 
 
 router.get('/connect', async (req: Request, res: Response, next: NextFunction) => {
   const code = getCode(req.url);
+  console.log('***CODE***', code)
 
   const accessToken = await getAccessToken(code);
+  console.log('***accessToken***', accessToken)
+
 
   const pageId = await getPageId(accessToken);
+  console.log('***pageID***', pageId)
+
 
   const pageAccessToken = await getPageAccessToken(accessToken, pageId);
+  console.log('***pageAccessToken***', pageAccessToken)
 
   const {followers_count, fan_count} = await getBasicStats(accessToken, pageId);
+  console.log('***followers_count***', followers_count, fan_count)
+
 
   const {media_count, media_list} = await getMedias(pageAccessToken, pageId);
+  console.log('***media_count***', media_count, media_list)
 
   const {demographics, geographics} = await getDemoGeo(pageAccessToken, pageId);
+  console.log('***demographics***', demographics, geographics)
 
   const data_intervals = await getIntervalsData(pageAccessToken, pageId);
+  console.log('***data_intervals***')
+
 
 
   const organized_data: FB_data = {
@@ -59,11 +71,11 @@ router.get('/connect', async (req: Request, res: Response, next: NextFunction) =
     data_intervals: data_intervals
   };
 
-  await postFBData(organized_data, req.headers.cookie).then(()=> {res.redirect('http://localhost:3000')})
+  await postFBData(organized_data, req.headers.cookie).then(()=> {res.redirect('http://localhost:3001')})
 
 })
 
 
-router.post("/update", updateFB, aggregateStat);
+router.post("/update", verifyHeaderJWT, updateFB, aggregateStat);
 
 export default router;
