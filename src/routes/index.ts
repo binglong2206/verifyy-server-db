dbea95c62e22dfcf07f6a8e1485cc1dceab16d87
jason <binglong2206@gmail.com>
2022-07-27 16:27:17 +0700

    firebase storage handler

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -33,116 +33,15 @@ import { checkUser } from "../middleware/checkUser";
 const router = express.Router();
 
 // next Function is automatically assigned by app.use()
-
-// Only for beta cus' have pre-defined slugs, should be dynamic to when user create account in prod
-router.get(
-  "/checkslug/:slug",
-  (req: Request, res: Response, next: NextFunction) => {
-    const { slug } = req.params;
-
-    if (!beta_users.find((e) => e.username === slug)) {
-      res.json({
-        redirect: {
-          destination: "/404",
-        },
-      });
-    }
-
-    res.json({
-      props: {
-        id: slug,
-        username: slug,
-      },
-    });
-  }
-);
-
-router.get("/", (req: Request, res: Response, next: NextFunction) => {
-  // const newUser = new User();
-  // newUser.firstname = "42";
-
-  // const newFB = new FB_account();
-  // newFB.like_counts = 777;
-  // newFB.user = newUser;
-  // newFB.demographics = { test1: "test1" };
-
-  // AppDataSource.manager.save(newUser);
-  // AppDataSource.manager.save(newFB);
-  // console.log("done");
-  res.send("homepage");
-});
-router.get("/json", async (req: Request, res: Response, next: NextFunction) => {
-  const fb = await FB_account.find({
-    relations: {
-      user: true,
-      medias: true,
-    },
-    where: {
-      // foreign columns are just nested object in the parent entity
-      user: {
-        id: 41,
-      },
-    },
-  });
-  const demographics = fb[0].demographics;
-  console.log(demographics["dog"]);
-  res.send(fb);
-});
-
-router.get("/many", async (req: Request, res: Response, next: NextFunction) => {
-  const fbaccount = await FB_account.findOne({
-    where: {
-      user: {
-        id: 41,
-      },
-    },
-  });
-  const media1 = new FB_media();
-  media1.like_count = 123;
-  media1.account = fbaccount;
-
-  AppDataSource.manager.save(media1);
-
-  console.log("done");
-  res.send("onetomany");
-});
-
-router.get(
-  "/cookie",
-  async (req: Request, res: Response, next: NextFunction) => {
-    console.log(req.headers);
-
-    // res.setHeader("Set-Cookie", [
-    //   cookie.serialize("accessToken", "accessToken", {
-    //     maxAge: 3.154e10,
-    //     httpOnly: true,
-    //     sameSite: true,
-    //     secure: true,
-    //     path: "/",
-    //   }),
-    //   cookie.serialize("refreshToken", "refreshToken", {
-    //     maxAge: 3.154e10,
-    //     httpOnly: true,
-    //     sameSite: true,
-    //     secure: true,
-    //     path: "/",
-    //   }),
-    // ]);
-    // console.log("cookie set");
-    res.end();
-  }
-);
-
-
 router.use('/oauth', oauthRouter)
 router.use("/dashboard", verifyCookieJWT, dashboardRouter);
-router.use("/youtube", youtubeRouter);
+router.use("/youtube", youtubeRouter); // Social routers have custom auth middleware inside
 router.use("/instagram", instagramRouter);
 router.use("/facebook", facebookRouter);
+router.use('/user', verifyCookieJWT, userRouter);
 router.get('/checkUser/:username', checkUser);
-router.use('/user', userRouter);
 
-
+// Test routes
 router.get("/data/:id", showUserData);
 router.get("/users", showUsers);
 router.post("/login", loginHandler);
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,9 +1,53 @@
 import express, { NextFunction, Request, Response } from 'express';
+import firebaseSDK from '../service/firebase'
 const router = express.Router();
 
+interface Locals{
+    id: number;
+    username: string
+}
 
-router.post('/profile', (req:Request, res: Response, next: NextFunction) => {
+router.post('/profile', async (req:Request, res: Response, next: NextFunction) => {
+    try {
+        const {id, username} = <Locals>res.locals
+        //@ts-ignore
+        const file = req.files.profile;
+        //@ts-ignore
+        const fileName = req.files.name;
 
+        if (file) {
+          const storageRef = 
+          const uploadTask = storageRef
+            .child(username + "/" + fileName)
+            .put(file, { contentType: file.type });
+    
+          uploadTask.on(
+            "state_change",
+            () => {
+              console.log('uploading');
+            },
+            (err:Error) => {
+              throw err
+            },
+            () => {
+              uploadTask.snapshot.ref.getDownloadURL().then((url: string) => {
+                
+                // Controller to post url to db
+
+
+                res.end();
+              });
+            }
+          );
+        } else {
+          throw new Error('issue uploading')
+        }
+    } catch(err){
+        console.error(err);
+        next(err)
+    }
+        
+      
 })
 
 
